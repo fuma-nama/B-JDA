8baf651cea70371a1506106ce74648a8e60eb564
SonMooSans <god63820869@gmail.com>
2022-08-01 16:54:53 +0800

    Move useless methods from LambdaBuilder to ComponentBuilder

--- src/main/kotlin/bjda/ui/core/Component.kt
+++ src/main/kotlin/bjda/ui/core/Component.kt
@@ -6,6 +6,8 @@ import bjda.ui.core.hooks.Delegate
 import bjda.ui.core.hooks.IHook
 import bjda.ui.types.*
 import bjda.utils.LambdaBuilder
+import bjda.utils.LambdaList
+import bjda.utils.build
 import net.dv8tion.jda.api.interactions.callbacks.IMessageEditCallback
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import kotlin.reflect.KProperty
@@ -30,6 +32,37 @@ fun <T: CProps<R>, R> T.init(init: T.() -> R): T {
 }
 
 open class ComponentBuilder : LambdaBuilder<AnyElement?>() {
+    /**
+     * Return item if condition is true, otherwise return null
+     */
+    inline fun <T: AnyElement> on(condition: Boolean, item: () -> T): T? {
+        return if (condition) item() else null
+    }
+
+    /**
+     * Return item if condition is false, otherwise return null
+     */
+    inline fun <T: AnyElement> not(condition: Boolean, item: () -> T): T? {
+        return if (!condition) item() else null
+    }
+
+    /**
+     * Return a list of items if condition is true
+     *
+     * Otherwise, return empty list
+     */
+    fun where(condition: Boolean, vararg items: AnyElement?): Array<out AnyElement?> {
+        return if (condition) items else arrayOfNulls(items.size)
+    }
+
+    /**
+     * Return a list of items if condition is true
+     *
+     * Otherwise, return a list filled with null with min size
+     */
+    fun where(condition: Boolean, items: LambdaList<AnyElement?>, min: Int): List<AnyElement?> {
+        return if (condition) items.build() else List(min) { null }
+    }
 
     /**
      * Add elements as a fragment
