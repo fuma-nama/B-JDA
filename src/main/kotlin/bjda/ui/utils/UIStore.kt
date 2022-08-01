2350c853965eb34ec5176bde2fcfcf9427465318
SonMooSans <god63820869@gmail.com>
2022-08-01 18:07:52 +0800

    Improve UIStore Util

--- src/main/kotlin/bjda/ui/utils/UIStore.kt
+++ src/main/kotlin/bjda/ui/utils/UIStore.kt
@@ -3,18 +3,36 @@ package bjda.ui.utils
 import bjda.ui.core.UI
 
 open class UIStore<T>: HashMap<T, UI>() {
-    fun getUI(key: T): UI? {
-        return this[key]
+    /**
+     * get UI instance or put the default value
+     */
+    fun getUI(key: T, createUI: () -> UI): UI {
+        return this.getOrPut(key, createUI)
     }
 
-    fun getUI(key: T, createUI: () -> UI): UI {
-        var ui = this[key]
-
-        if (ui == null) {
-            ui = createUI()
-            this[key] = ui
-        }
+    /**
+     * Replace and Destroy the old instance with the new one
+     *
+     * @return Destroyed UI
+     */
+    fun replaceUI(key: T, new: UI): UI? {
+        val ui = this.replace(key, new)
+        ui?.destroy()
 
         return ui
+    }
+
+    /**
+     * Remove and Destroy UI
+     *
+     * @return True if the key exists
+     */
+    fun destroy(key: T): Boolean {
+        val ui = this.remove(key)
+            ?: return false
+
+        ui.destroy()
+
+        return true
     }
 }
