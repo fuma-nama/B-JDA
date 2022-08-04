92be99bed83f37809d2bdb7db9c899c779071acd
SonMooSans <god63820869@gmail.com>
2022-08-04 20:22:38 +0800

    Remove useless classes

--- src/main/kotlin/bjda/utils/Check.kt
+++
@@ -1,9 +0,0 @@
-package bjda.utils
-
-    fun allEmpty(vararg lists: Collection<*>): Boolean {
-        return lists.all { it.isEmpty() }
-    }
-
-    fun notEmpty(vararg lists: Collection<*>): Boolean {
-        return lists.all {it.isNotEmpty()}
-    }
