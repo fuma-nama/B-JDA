e950ac18c57566170c0e00d4d9a1a3cdd42c519f
SonMooSans <god63820869@gmail.com>
2022-08-06 16:15:27 +0800

    Rewrite Builders

--- src/main/kotlin/bjda/ui/utils/ComponentBuilder.kt
+++ src/main/kotlin/bjda/ui/utils/ComponentBuilder.kt
@@ -2,7 +2,10 @@ package bjda.ui.utils
 
 import bjda.ui.component.Fragment
 import bjda.ui.component.utils.Builder
+import bjda.ui.core.*
 import bjda.ui.types.AnyElement
+import bjda.ui.types.Children
+import bjda.utils.Blocking
 import bjda.utils.LambdaBuilder
 import bjda.utils.LambdaList
 import bjda.utils.build
@@ -10,26 +13,97 @@ import net.dv8tion.jda.api.interactions.components.ActionRow
 
 open class ComponentBuilder : LambdaBuilder<AnyElement?>() {
     /**
-     * Return item if condition is true, otherwise return null
+     * Create and add Component as children
      */
-    inline fun <T: AnyElement> on(condition: Boolean, item: () -> T): T? {
-        return if (condition) item() else null
+    operator fun<E : ElementImpl<P>, P : CProps<C>, C : Any> ElementFactory<E, P, C>.invoke(init: P.() -> C): E {
+        return create(init).also {
+            + it
+        }
+    }
+
+    /**
+     * Create and add Component as children
+     */
+    operator fun<E : ElementImpl<P>, P : CProps<C>, C : Any> ElementFactory<E, P, C>.invoke(init: P.() -> Unit, children: C): E {
+        return create { init(this); children }.also {
+            + it
+        }
+    }
+
+    /**
+     * Create and add Component as children
+     */
+    operator fun<P : CProps<C>, C: Any> FComponentConstructor<P, C>.invoke(props: P.() -> C): FComponent<P> {
+        val comp = this..(props)
+        + comp
+
+        return comp
+    }
+
+    /**
+     * Add children when condition is true
+     *
+     * you cannot add more than two children in a condition
+     */
+    fun `if`(condition: Boolean, children: Children) {
+
+        if (condition) {
+            val before = elements.size
+            children(this)
+
+            if (before + 1 < elements.size) {
+                error("You can't add more than two elements in a condition")
+            }
+        } else {
+            + null
+        }
+    }
+
+    /**
+     * Add children when condition is true. Otherwise, Add the default children
+     *
+     * you cannot add more than two children in a condition
+     */
+    fun ifElse(condition: Boolean, children: Children, default: Children) {
+
+        if (condition) {
+            val before = elements.size
+            children(this)
+
+            if (before + 1 < elements.size) {
+                error("You can't add more than two elements in a condition")
+            }
+        } else {
+            val before = elements.size
+            default(this)
+
+            if (before + 1 < elements.size) {
+                error("You can't add more than two elements in a condition")
+            }
+        }
+    }
+
+    /**
+     * Add item if condition is true, otherwise add null
+     */
+    inline fun <T: AnyElement> on(condition: Boolean, item: Blocking.() -> T) {
+        + if (condition) item(Blocking.default) else null
     }
 
     /**
      * Return item if condition is false, otherwise return null
      */
-    inline fun <T: AnyElement> not(condition: Boolean, item: () -> T): T? {
-        return if (!condition) item() else null
+    inline fun <T: AnyElement> not(condition: Boolean, item: Blocking.() -> T) {
+        + if (!condition) item(Blocking.default) else null
     }
 
     /**
-     * Return a list of items if condition is true
+     * Add a list of items if condition is true
      *
-     * Otherwise, return empty list
+     * Otherwise, add a list of nulls
      */
-    fun where(condition: Boolean, vararg items: AnyElement?): Array<out AnyElement?> {
-        return if (condition) items else arrayOfNulls(items.size)
+    fun where(condition: Boolean, vararg items: AnyElement?) {
+        + if (condition) items else arrayOfNulls(items.size)
     }
 
     /**
@@ -37,8 +111,8 @@ open class ComponentBuilder : LambdaBuilder<AnyElement?>() {
      *
      * Otherwise, return a list filled with null with min size
      */
-    fun where(condition: Boolean, items: LambdaList<AnyElement?>, min: Int): List<AnyElement?> {
-        return if (condition) items.build() else List(min) { null }
+    fun where(condition: Boolean, items: LambdaList<AnyElement?>, min: Int) {
+        + if (condition) items.build() else List(min) { null }
     }
 
     /**
