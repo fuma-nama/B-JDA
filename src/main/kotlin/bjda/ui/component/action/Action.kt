750d8b56581dfcf6fe93a2c5db72c4b83ac1ea2f
SonMooSans <god63820869@gmail.com>
2022-08-19 18:16:17 +0800

    Add DslBuilder annotation to Action

--- src/main/kotlin/bjda/ui/component/action/Action.kt
+++ src/main/kotlin/bjda/ui/component/action/Action.kt
@@ -1,7 +1,9 @@
 package bjda.ui.component.action
 
+import bjda.utils.DslBuilder
 import net.dv8tion.jda.api.interactions.components.ActionComponent
 
+@DslBuilder
 interface Action {
     val id: String?
     fun build(): ActionComponent
